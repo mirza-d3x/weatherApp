77a451f3725223e81f332d393589da9763cdefaa
mirza-d3x <mohammedmirsa010@gmail.com>
2022-12-31 07:25:55 +0530

    bloc registerd

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:weather_lilac/Application/Bloc/WeatherDb/weather_db_bloc.dart';
 import 'package:weather_lilac/Infrastructure/Api/api_functions.dart';
+import 'package:weather_lilac/Model/get_weather_data_model.dart';
 import 'Application/Bloc/ApiData/api_data_bloc.dart';
 import 'Presentation/SplashScreen/splash_screen.dart';
 import 'package:flutter_bloc/src/bloc_provider.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  if (!Hive.isAdapterRegistered(GetWeatherDataModelAdapter().typeId)) {
+    Hive.registerAdapter(GetWeatherDataModelAdapter());
+  }
   runApp(const MyApp());
 }
 
@@ -14,8 +22,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Apis apis = Apis();
-    return BlocProvider(
-      create: (context) => ApiDataBloc(apis),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => ApiDataBloc(apis),
+        ),
+        BlocProvider(
+          create: (context) => WeatherDbBloc(),
+        ),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
